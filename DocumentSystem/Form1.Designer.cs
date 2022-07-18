e52cbea3d43bcbfe82ff5fca8b141b1e60ac06f5
Swapnil Bhojane <swapnilbhojane@outlook.com>
2022-07-18 23:17:22 +0530

    initial commit

---
+++ DocumentSystem/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace DocumentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(479, 51);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(578, 51);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(71, 54);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(356, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(71, 141);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.Size = new System.Drawing.Size(582, 152);
+            this.dgvDocuments.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(71, 95);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dgvDocuments);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ DocumentSystem/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            txtFilePath.Text = dlg.FileName;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFile(txtFilePath.Text);
+            MessageBox.Show("Saved");
+        }
+        private void SaveFile(string filePath)
+        {
+            using (Stream stream = File.OpenRead(filePath))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                var fi = new FileInfo(filePath);
+                string extn = fi.Extension;
+                string name = fi.Name;
+
+                string query = "INSERT INTO Documents(FileName,Data,Extension) VALUES(@name,@data,@extn)";
+
+                using (SqlConnection cn = GetConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = name;
+                    cmd.Parameters.Add("@data", SqlDbType.VarBinary).Value = buffer;
+                    cmd.Parameters.Add("@extn", SqlDbType.Char).Value = extn;
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=DESKTOP-6CU1O1O\SQLEXPRESS;Database=DocumentSystem;Integrated Security=true;");
+        }
+        private void Form1_Load(object sender,EventArgs e)
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            using (SqlConnection cn = GetConnection())
+            {
+                string query = "SELECT ID,FileName,Extension FROM Documents";
+                SqlDataAdapter adp = new SqlDataAdapter(query, cn);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    dgvDocuments.DataSource = dt;
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var selectedRow = dgvDocuments.SelectedRows;
+            foreach(var row in selectedRow)
+            {
+                    int id = (int)((DataGridViewRow)row).Cells[0].Value;
+                    OpenFile(id);
+            }
+        }
+        private void OpenFile(int id)
+        {
+            using (SqlConnection cn =  GetConnection())
+            {
+                string query = "SELECT data,FileName,Extension FROM Documents WHERE ID=@id";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.Add("@id",SqlDbType.Int).Value=id;
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if(reader.Read())
+                {
+                    var name = reader["FileName"].ToString();
+                    var data = (byte[])reader["data"];
+                    var extn = reader["Extension"].ToString();
+                    var newFileName = name.Replace(extn, DateTime.Now.ToString("ddMMyyyyhhmmss")) +extn;
+                    File.WriteAllBytes(newFileName,data);
+                    System.Diagnostics.Process.Start(newFileName);
+                }
+            }
+        }
+
+    }
+}
